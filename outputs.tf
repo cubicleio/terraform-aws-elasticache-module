4794ee078fc06d03911309d31293c5e9190626a2
Josh Dvir <shuky.dvir@gmail.com>
2017-04-05 14:50:17 +0100

    outputs

--- outputs.tf
+++ outputs.tf
@@ -1,10 +1,6 @@
-output "security_group_id" {
-  value = "${aws_security_group.security_groups_for_cache.id}"
-}
-
 output "redis_address" {
-  value = "${aws_elasticache_cluster.elastic_cache.cluster_address}"
+  value = "${aws_elasticache_cluster.elasticache.cluster_address}"
 }
 output "port" {
-  value = "${aws_elasticache_cluster.elastic_cache.port}"
+  value = "${aws_elasticache_cluster.elasticache.port}"
 }
