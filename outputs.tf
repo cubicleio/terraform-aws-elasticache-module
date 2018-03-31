831653945156d288b7c178d5415d2940bda65a40
Pierre-Luc Maheux <pierreluc.maheux@gmail.com>
2018-03-31 15:44:08 +0200

    Change output variables

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,6 @@
 output "redis_nodes" {
   value = "${aws_elasticache_cluster.elasticache.cache_nodes}"
 }
-output "redis_configuration_endpoint" {
-  value = "${aws_elasticache_cluster.elasticache.configuration_endpoint}"
-}
 output "port" {
   value = "${aws_elasticache_cluster.elasticache.port}"
 }
